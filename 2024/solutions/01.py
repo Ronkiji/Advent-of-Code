d0df1351f0fcff1b609a9eb8c28253c5174a3c68
Ronkiji <ssun.ronald@gmail.com>
2024-12-01 00:16:38 -0500

    2024 day 01

--- 2024/solutions/01.py
+++ 2024/solutions/01.py
@@ -1,2 +1,16 @@
-inputs = [line.rstrip('\n') for line in open("inputs/01.txt")]
-print(" ".join(inputs))
+parse = [(line.rstrip('\n')) for line in open("inputs/01.txt")]
+left = [int(key) for key, val in (line.split() for line in parse)]
+right = [int(val) for key, val in (line.split() for line in parse)]
+
+left, right = sorted(left), sorted(right)
+
+def one():
+    return sum(abs(x-y) for x, y in zip(left, right))
+
+def two():
+    from collections import Counter
+    d = Counter(right)
+    return sum(y * d[y] for y in left if y in d)
+
+print(one())
+print(two())
